3478c216bb2b0792193550120d4a3064ad7a1396
Awangrond <kirill.karpushin.2004@mail.ru>
2023-04-13 22:32:41 +0300

    minDiv

---
+++ Минимальный делитель/Program.cs
@@ -0,0 +1,19 @@
+﻿int scan=0, minDiv=0, mod=1, i=1;
+
+Console.WriteLine ("Напишите число:");
+scan = Convert.ToInt32(Console.ReadLine());
+
+    while (mod != 0)
+    {   
+        if (scan == 0)
+        {
+            i = 0;
+        }
+
+        i++;
+        mod = scan % i;
+    }
+
+minDiv = i;
+Console.WriteLine("Минимальный делитель");
+Console.WriteLine(minDiv);
